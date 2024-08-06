9d30974b0cd19279f744a45bc7a8cf347ab8dd2e
AlfredoMAramburo <alfredo.0108ma@gmail.com>
2024-08-05 21:46:53 -0600

    Updates

---
+++ _next/static/-wArefzHr-mSTMiHomS2h/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-a957b5e9e6267e9e.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
